957b2732ed82c1ff05362fb279cecdcadc84cea6
k-Dobosz <kdobosz02@gmail.com>
2021-10-16 01:23:43 +0200

    Fix cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express'
 import logger from 'morgan'
 import bodyParser from 'body-parser'
 import mongo from './database/db'
+import cors from 'cors'
 
 import usersRouter from './routes/users'
 
@@ -11,18 +12,14 @@ const db = mongo()
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors({
+    origin: '*',
+    methods: 'GET, PUT, PATCH, POST, DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+}))
 
 app.use('/api/v1/users', usersRouter)
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    if(req.method === "OPTIONS") {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-        return res.status(200).json({})
-    }
-    next()
-})
 
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
     res.status(404).json({
