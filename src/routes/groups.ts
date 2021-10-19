da37772723fe7c54665f4d89ca92e80b977b4a7c
k-Dobosz <kdobosz02@gmail.com>
2021-10-19 23:33:44 +0200

    Add new routes and fix joining group

--- src/routes/groups.ts
+++ src/routes/groups.ts
@@ -17,6 +17,21 @@ router.get('/', auth, async (req: Request, res: Response, next: NextFunction) =>
     }
 })
 
+router.get('/:groupId', auth, async (req: Request, res: Response, next: NextFunction) => {
+    const groupId = req.params.groupId
+
+    try {
+        const group = await Group.findOne({ _id: groupId })
+
+        if (!group)
+            return next(new AppError('No group found', 404))
+
+        res.status(200).send({ group })
+    } catch (e) {
+        next(e)
+    }
+})
+
 router.post('/', auth, async (req: Request, res: Response, next: NextFunction) => {
     const group = new Group({
         name: req.body.name,
@@ -31,7 +46,7 @@ router.post('/', auth, async (req: Request, res: Response, next: NextFunction) =
 
         const joinToken = await group.generateJoinToken()
 
-        res.status(201).send({ joinToken })
+        res.status(201).send({ _id: group._id, joinToken })
     } catch (e) {
         next(e)
     }
@@ -44,12 +59,53 @@ router.get('/join/:joinToken', auth, async (req: Request, res: Response, next: N
         const group = await Group.findOne({ joinToken })
 
         if (!group) {
-            return next(new AppError('Your invitation token is invalid', 404))
+            return next(new AppError('Your invitation token is invalid.', 404))
+        }
+
+        const users: Array<any> = group.users
+
+        if (users.some(item => item.userId.toString() == req.user._id.toString())) {
+            return next(new AppError('You already are in this group.', 404))
         }
 
         group.users = group.users.concat({ userId: req.user._id })
-
         group.save()
+
+        res.status(200).send({ 
+            _id: group._id, 
+            name: group.name,
+            subject: group.subject,
+            emoji: group.emoji,
+            users: group.users,
+            creator: group.creator
+        })
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.patch('/:groupId', auth, async (req: Request, res: Response, next: NextFunction) => {
+    const groupId = req.params.groupId
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'emoji', 'joinToken']
+    const isValid = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValid) {
+        return next(new AppError('Invalid fields', 400))
+    }
+
+    try {
+        const group = await Group.findOneAndUpdate({ _id: groupId }, req.body, { new: true })
+
+        res.status(200).send(group)
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.delete('/', auth, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const group = await Group.deleteOne({ _id: req.body.groupId })
 
         res.status(200).send({})
     } catch (e) {
